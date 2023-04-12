a665463dacbc4006187983c826084a81f010aef0
S0L3X <s0l3x1@protonmail.com>
2023-04-12 19:08:57 +0100

    First commit

---
+++ client/src/components/ColorPicker.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+
+const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <div className='absolute left-full ml-3'>
+      <SketchPicker 
+        color={snap.color}
+        disableAlpha
+        presetColors={[
+          // '#000000', '#ffffff', '#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff'
+          '#ccc', '#efbd4e', '#80c670', '#726de8', '#353934', '#2ccce4', '#ff8a65', '#7098da', '#c19277', '#ff96ad', '#512314', '#5f123d'
+        ]}
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
+  )
+}
+
+export default ColorPicker
